5ec5888e4c4d31a2ce3afbdfd78211220f9e4054
Fivos Iliadis <eternallistener@gmail.com>
2020-10-31 18:55:22 +0200

    Syncing with laptop

--- app/src/main/java/com/example/onomazwopragmaproject/MainActivity.kt
+++ app/src/main/java/com/example/onomazwopragmaproject/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.onomazwopragmaproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            var intent: Intent = Intent(this, HostOrJoinRoomActivity::class.java)
+            startActivity(intent)
+        }
+
+        var button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            // ...
+        }
+
+
     }
 }
