51b1f1d71d9acb19e6cb5ea3422010c3d207f065
arkountos <eternallistener@gmail.com>
2020-11-01 00:27:54 +0200

    Particles on start screen done

--- app/src/main/java/com/example/onomazwopragmaproject/MainActivity.kt
+++ app/src/main/java/com/example/onomazwopragmaproject/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.onomazwopragmaproject
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.ViewTreeObserver
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.plattysoft.leonids.ParticleSystem
+import kotlin.math.pow
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,23 +31,87 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+//        val viewTreeObserver: ViewTreeObserver = rootView.viewTreeObserver
+//        if (viewTreeObserver.isAlive) {
+//            viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+//                override fun onGlobalLayout() {
+//                    rootView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+//
+//                    var particleSystem = ParticleSystem(this@MainActivity, 500, R.drawable.compass, 20000)
+//                        .setSpeedRange(0.02f, 0.1f)
+//                        .setRotationSpeed(40F)
+//                        .setScaleRange(0.3f, 0.4f)
+//                        .emit((0..200).random(), (0..200).random(), 1)
+//
+//
+//                }
+//            })
+//        }
+
+        // ------------------------------------------------------------------------------------------------------------------
+        // When the app starts, we create a thread that is responsible for updating the main currency *view* every X seconds.
+        // This thread is posting changes for the UI thread.
+        // PROCEED WITH CAUTION WHEN CHANGING THIS PART OF THE CODE
+        // ------------------------------------------------------------------------------------------------------------------
         val rootView = findViewById<ConstraintLayout>(R.id.root_constraint_layout)
-        val viewTreeObserver: ViewTreeObserver = rootView.viewTreeObserver
-        if (viewTreeObserver.isAlive) {
-            viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
-                override fun onGlobalLayout() {
-                    rootView.viewTreeObserver.removeOnGlobalLayoutListener(this)
-//                    viewWidth = root_view.width
-//                    viewHeight = root_view.height
-                    var particleSystem = ParticleSystem(this@MainActivity, 500, R.drawable.ic_launcher_foreground, 10000)
-                        .setSpeedRange(0.2f, 0.5f)
-                        .emit(rootView, 50)
-
+        val mainDelay : Long = 1000
+        val handler = Handler()
+        val runnable: Runnable = object : Runnable {
+            override fun run() {
+                var x: Int = 0
+                var y: Int = 0
+                when ((0..3).random()) {
+                    0 -> {
+                        x = -40
+                        y = (0..rootView.height).random()
+                        }
+                    1 -> {
+                        x = (0..rootView.width).random()
+                        y = -40
+                        }
+                    2 -> {
+                        x = rootView.width + 40
+                        y = (0..rootView.height).random()
+                    }
+                    3 -> {
+                        x = (0..rootView.width).random()
+                        y = rootView.height + 400
+                    }
                 }
-            })
+
+                var floating_item: Drawable = resources.getDrawable(R.drawable.compass)
+                when ((0..9).random()) {
+                    0 -> floating_item = resources.getDrawable(R.drawable.compass)
+                    1 -> floating_item = resources.getDrawable(R.drawable.eraser)
+                    2 -> floating_item = resources.getDrawable(R.drawable.highlighter)
+                    3 -> floating_item = resources.getDrawable(R.drawable.paper_clip)
+                    4 -> floating_item = resources.getDrawable(R.drawable.pencil)
+                    5 -> floating_item = resources.getDrawable(R.drawable.pens)
+                    6 -> floating_item = resources.getDrawable(R.drawable.protractor)
+                    7 -> floating_item = resources.getDrawable(R.drawable.ruler)
+                    8 -> floating_item = resources.getDrawable(R.drawable.send)
+                    9 -> floating_item = resources.getDrawable(R.drawable.sharpener)
+                }
+
+                rootView.post {
+                    Log.d("xy", "$x $y")
+                    var particleSystem = ParticleSystem(
+                        this@MainActivity,
+                        500,
+                        floating_item,
+                        20000
+                    )
+//                        .setSpeedModuleAndAngleRange(0.02f, 0.1f, 0, 180)
+                        .setSpeedRange(0.02f, 0.1f)
+                        .setRotationSpeed(40F)
+                        .setScaleRange(0.2f, 0.2f)
+                        .emit(x, y, 1, 1000)
+                }
+                handler.postDelayed(this, mainDelay)
+            }
         }
-
-
-
+        handler.postDelayed(runnable, mainDelay)
+        // ------------------------------------------------------------------------------------------------------------------
+        // ------------------------------------------------------------------------------------------------------------------
     }
 }
