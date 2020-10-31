4d20b9d0adc48f811ed458d8c4cc827f0022bde3
arkountos <eternallistener@gmail.com>
2020-10-31 20:27:56 +0200

    Particle library working

--- app/src/main/java/com/example/onomazwopragmaproject/MainActivity.kt
+++ app/src/main/java/com/example/onomazwopragmaproject/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.onomazwopragmaproject
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewTreeObserver
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.plattysoft.leonids.ParticleSystem
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +25,24 @@ class MainActivity : AppCompatActivity() {
         button2.setOnClickListener {
             // ...
         }
+
+
+        val rootView = findViewById<ConstraintLayout>(R.id.root_constraint_layout)
+        val viewTreeObserver: ViewTreeObserver = rootView.viewTreeObserver
+        if (viewTreeObserver.isAlive) {
+            viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    rootView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+//                    viewWidth = root_view.width
+//                    viewHeight = root_view.height
+                    var particleSystem = ParticleSystem(this@MainActivity, 500, R.drawable.ic_launcher_foreground, 10000)
+                        .setSpeedRange(0.2f, 0.5f)
+                        .emit(rootView, 50)
+
+                }
+            })
+        }
+
 
 
     }
